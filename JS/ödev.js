111e069737427fb33e5fea3c8f87f850b681243e
BuketMutlu <105936996+BuketMutlu@users.noreply.github.com>
2022-05-23 00:48:57 +0300

    Update ödev.js

--- JS/ödev.js
+++ JS/ödev.js
@@ -1,3 +1,7 @@
+function giriş()
+{
+    alert("SAYFAMA HOŞ GELDİNİZ");
+}
 function süre()
 {
     var isorange=false;
@@ -50,4 +54,56 @@ function süre2()
             isorange=true;
         }
     },1000)
+}
+
+function renk()
+{
+    var renk=document.getElementById("menüler");
+   
+    renk.style.borderColor="brown";
+}
+function nesne()
+{
+    var nesne={
+               adı: "BUKET",
+               Soyad: "MUTLU",
+               Bölüm:"bilgisayar mühendisligi"
+
+    }
+    console.log(nesne);
+}
+function merhaba(obje)
+{
+    obje.innerHTML="Tıkladınız!";
+}
+function değiştirme()
+{
+    var h5=document.getElementsByTagName('h5')[0];
+    h5.textContent="taze kavrulmuş isteğinize özel öğütülmüş dünya kahvelerini";
+    console.log(h5);
+}
+function saat()
+{
+    var date  = new Date();
+    console.log(date);
+   
+
+}
+function ydeğiş()
+{
+    document.getElementById("biz").innerHTML="çok teşekkürler";
+}
+function index()
+{
+    var str = "Visit W3Schools!";
+    var n = str.search("W3Schools"); //6 döner
+    console.log(n);
+}
+function etiket()
+{
+    document.querySelector("a");
+    var a=document.querySelector("a");
+    a.setAttribute("href","https://www.google.com")
+    console.log(a);
+
 }
